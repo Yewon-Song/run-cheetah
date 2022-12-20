420e03ae0b1351d1495243f6c94f74c278b0c720
Yewon-Song <88762541+Yewon-Song@users.noreply.github.com>
2022-12-20 19:48:49 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,5 +1,15 @@
+##***입력받은 숫자만큼 반복하기
 num = int(input( ))
 i = 0 
 while i < num:
-    print(' ',num)
-    i += 1
+     print(' ',num)
+     i += 1
+
+##정수 한 개를 입력받아 1부터 입력받은 수까지 각각에 대해 제곱구함
+
+num2 = int(input())
+k=1
+while k <= num2:
+    print(k, k*k)
+    k += 1
+
